a6bbcbbb54d38e1f610f4f7ea5644d52579af34e
Trevor Bursach <bursacht@gmail.com>
2020-10-14 16:02:43 -0600

    changes deployment

---
+++ ColorMatch/ColorMatch/Controllers/ViewControllers/MainScreenViewController 2.swift
@@ -0,0 +1,28 @@
+//
+//  MainScreenViewController.swift
+//  ColorMatch
+//
+//  Created by Shean Bjoralt on 10/14/20.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+    
+    //MARK: - Outlets
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - Actions
+    
+    
+    
+    
+
+
+}
---
+++ ColorMatch/ColorMatch/Model/UserScore 2.swift
@@ -0,0 +1,19 @@
+//
+//  userScore.swift
+//  ColorMatch
+//
+//  Created by Owen Barrott on 10/14/20.
+//
+
+import Foundation
+
+class UserScore {
+    
+    let name: String
+    let score: Int
+    
+    init (name: String, score: Int) {
+        self.name = name
+        self.score = score
+    }
+}
