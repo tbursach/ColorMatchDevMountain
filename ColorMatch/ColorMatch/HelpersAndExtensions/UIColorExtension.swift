768421e12cb5c89d3297d5fccb9cee68de95076c
Shean Bjoralt <shean@datamanifold.com>
2020-10-15 11:38:53 -0600

    adds random color extension

---
+++ ColorMatch/ColorMatch/HelpersAndExtensions/UIColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExtension.swift
+//  ColorMatch
+//
+//  Created by Shean Bjoralt on 10/15/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func getRandomColor() -> UIColor {
+        let colors = [UIColor.red, UIColor.blue, UIColor.yellow, UIColor.black, UIColor.white, UIColor.green, UIColor.purple, UIColor.brown, UIColor.orange]
+        let randomNumber = arc4random_uniform(UInt32(colors.count))
+        
+        return colors[Int(randomNumber)]
+    }
+}
